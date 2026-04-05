c670fcf1392bc17fbbc91f456e3e55670b3c2656
Nekruz <nekruzh9@gmail.com>
2026-04-06 00:57:31 +0300

    -работа с ts типы, интнрыецсы -работа с ассинхронным кодом -useFeatch -реализовал регистрация -потренеровался с slot

---
+++ app/components/testSlot.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// проверка возможности слотов
+</script>
+
+<template>
+   <slot>
+    <h1>привет</h1>
+   </slot>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/registration.vue
+++ app/pages/registration.vue
@@ -1,10 +1,53 @@
 <script setup lang="ts">
+import { useUrl } from '~/store/url';
+
+// роутер
 let router = useRouter();
+
+// state 
+let url = useUrl();
 
 // тут мы явно указываем ему какой лейаут выбрать для этой страницы
 definePageMeta({
     layout: "entrance-layout"
 });
+
+// Тип для user
+type TUser = {
+    name: string,
+    surname: string,
+    email: string,
+    password: string,
+    role: "admin" | "user"
+}
+// переменные из v-model
+let user: TUser = reactive({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    role:"user"
+})
+
+
+// useFeatch - добавление пользователя в бд
+let {data, refresh: addUserRefresh, error: addUserError} = await useFetch(url.userUrl, {
+    immediate: false,
+    method: "post",
+    body: user
+});
+let addUser =  async () => {
+    if(addUserError.value){
+        alert(addUserError.value.message);
+        return;
+    }
+    else if(user.name.length <= 0 || user.surname.length <= 0 || user.email.length <= 0 || user.password.length <= 0){
+        alert("не все поля заполнены");
+        return;
+    }
+    addUserRefresh();
+    await router.push("/entrance");
+}
 
 </script>
 
@@ -14,20 +57,20 @@ definePageMeta({
             <h1>Регистрация</h1>
 
             <h2>Имя</h2>
-            <input type="text">
+            <input v-model="user.name" type="text">
 
             <h2>Фамилия</h2>
-            <input type="text">
+            <input v-model="user.surname" type="text">
 
             <h2>Логин</h2>
-            <input type="email">
+            <input v-model="user.email" type="email">
 
             <h2>Пароль</h2>
-            <input type="password">
+            <input v-model="user.password" type="password">
 
             <section class="btms">
                 <UIcomponentsButton @click="router.push('/entrance')" value="Вход" color="#000" background="white"/>
-                <UIcomponentsButton value="Регистрация" color="#fff" background="rgb(39, 39, 39)"/>
+                <UIcomponentsButton @click="addUser" value="Регистрация" color="#fff" background="rgb(39, 39, 39)"/>
             </section>
         </div>
     </section>
